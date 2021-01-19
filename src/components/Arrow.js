c8a8b34442063a0c6c24b1f14abbfc311a5fa120
Lennyk <lennyk.macedo@gcbinvestimentos.com>
2021-01-19 11:50:32 -0300

    ajuste espaçamento arrow

--- src/components/Arrow.js
+++ src/components/Arrow.js
@@ -19,21 +19,21 @@ function Arrow({ className, onClick, inverted, label, disabled }) {
       {label ? (
         <Label>{label}</Label>
       ) : (
-        <svg viewBox="0 0 18.4 14.4">
-          <path
-            d={
-              inverted
-                ? 'M17 7.2H1M10.8 1L17 7.2l-6.2 6.2'
-                : 'M1.4 7.2h16M7.6 1L1.4 7.2l6.2 6.2'
-            }
-            fill="none"
-            stroke="currentColor"
-            strokeWidth="2"
-            strokeLinecap="round"
-            strokeMiterlimit="10"
-          />
-        </svg>
-      )}
+          <svg viewBox="0 0 18.4 14.4">
+            <path
+              d={
+                inverted
+                  ? 'M17 7.2H1M10.8 1L17 7.2l-6.2 6.2'
+                  : 'M1.4 7.2h16M7.6 1L1.4 7.2l6.2 6.2'
+              }
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeMiterlimit="10"
+            />
+          </svg>
+        )}
     </SvgButton>
   )
 }
@@ -49,7 +49,8 @@ Arrow.propTypes = {
 export default styled(Arrow)`
   color: ${props => (props.disabled ? '#caccce' : '#646464')};
 
-  ${props => (props.inverted ? 'margin-left: 24px;' : 'margin-right: 24px;')};
+  ${props => (props.inverted ? 'margin-left: 10px;' : 'margin-right: 10px;')};
+  ${props => (props.inverted ? 'margin-right: 12px' : 'margin-left: 12px')}
   ${props =>
     !props.label &&
     `
